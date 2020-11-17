0ad4e6de74b2fea60d9196188acd1e36b8119965
Manfredi <mrmanfredi228@gmail.com>
2020-11-17 17:51:11 +0200

    Config files for JSF added.

--- src/main/java/com/example/feedback/FeedbackApplication.java
+++ src/main/java/com/example/feedback/FeedbackApplication.java
@@ -1,13 +1,38 @@
 package com.example.feedback;
 
+import org.ocpsoft.rewrite.servlet.RewriteFilter;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication
-public class FeedbackApplication {
+import javax.faces.webapp.FacesServlet;
+import javax.servlet.DispatcherType;
+import java.util.EnumSet;
+
+@EnableAutoConfiguration
+@ComponentScan({"com.example.feedback"})
+public class FeedbackApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(FeedbackApplication.class, args);
     }
 
+    @Bean
+    public ServletRegistrationBean servletRegistrationBean() {
+        FacesServlet servlet = new FacesServlet();
+        return new ServletRegistrationBean(servlet, "*.jsf");
+    }
+
+    @Bean
+    public FilterRegistrationBean rewriteFilter() {
+        FilterRegistrationBean rwFilter = new FilterRegistrationBean(new RewriteFilter());
+        rwFilter.setDispatcherTypes(EnumSet.of(DispatcherType.FORWARD, DispatcherType.REQUEST,
+                DispatcherType.ASYNC, DispatcherType.ERROR));
+        rwFilter.addUrlPatterns("/*");
+        return rwFilter;
+    }
 }
